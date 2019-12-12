e05c7ddae58469d3c4b47444ce0d77cdf4e067dd
Jefferson Silva <jefferson.ls563@gmail.com>
2019-12-12 19:00:01 -0300

    req

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth import login
 
 def login_views(request):
@@ -8,7 +8,18 @@ def login_views(request):
         if form.is_valid():
             user = form.get_user()
             login(request, user)
-            return redirect('livro:index')
+            return redirect('livros:index')
     else:
         form = AuthenticationForm()
     return render(request, 'accounts/login.html', {'form': form})
+
+def signup_views(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('livros:index')
+    else:
+        form = UserCreationForm()
+    return render(request, 'accounts/signup.html', {'form': form})
